917e84cd67d1ffdfa03d165ad4b342661d61837f
SupremoIntra <daniele.intra@studenti.unimi.it>
2024-02-26 11:44:11 +0100

    26_02_2024

--- client_rs.py
+++ client_rs.py
@@ -3,7 +3,7 @@ import os
 import subprocess
 import sys
 
-SERVER_HOST = "204.216.222.203"
+SERVER_HOST = "129.152.11.161"
 SERVER_PORT = 5003
 BUFFER_SIZE = 1024 * 128 # 128KB max size of messages, feel free to increase
 # separator string for sending 2 messages in one go
